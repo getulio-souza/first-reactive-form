4b52d6913f364d8ad74358eabc23f047d38b18e4
getulio.souza <getulio.souza@evolutix.com.br>
2023-03-29 15:48:17 -0300

    adding-form-control-to-inputs

--- reactive-form/src/app/first-form/first-form.component.ts
+++ reactive-form/src/app/first-form/first-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-first-form',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FirstFormComponent implements OnInit {
 
+  title = "ReactiveForms";
+  reactiveForm: FormGroup;
+
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.reactiveForm = new FormGroup({
+      firstName: new FormControl(null),
+      lastName: new FormControl(null),
+      email: new FormControl(null),
+      password: new FormControl(null),
+      country: new FormControl(null)
+    });
   }
 
 }
